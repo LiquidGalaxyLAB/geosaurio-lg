94475fcc1dc4bcdc8040f9bbc483979307398977
joseporiolse <joseporiolse@gmail.com>
2026-06-03 10:29:08 +0200

    Fix analyzer issue

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -307,7 +307,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       thumbVisibility: true,
                       child: ListView.separated(
                         itemCount: visibleCountries.length,
-                        separatorBuilder: (_, __) => const SizedBox(height: 10),
+                        separatorBuilder: (_, _) => const SizedBox(height: 10),
                         itemBuilder: (context, index) {
                           final country = visibleCountries[index];
 
