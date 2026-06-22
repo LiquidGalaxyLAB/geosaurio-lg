c820e5cce6646017164ad50360fa63fb02df8d3a
joseporiolse <joseporiolse@gmail.com>
2026-06-22 11:51:38 +0200

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+
 import 'package:geosaurio/main.dart';
+import 'package:geosaurio/services/lg_service.dart';
 
 void main() {
   testWidgets('GeoSaurioApp smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const GeoSaurioApp());
+    await tester.pumpWidget(
+      ChangeNotifierProvider<LgService>(
+        create: (_) => LgService(),
+        child: const GeoSaurioApp(),
+      ),
+    );
 
-    await tester.pumpAndSettle(const Duration(seconds: 6));
+    await tester.pump(const Duration(seconds: 6));
+    await tester.pump();
 
     expect(find.byType(GeoSaurioApp), findsOneWidget);
   });
